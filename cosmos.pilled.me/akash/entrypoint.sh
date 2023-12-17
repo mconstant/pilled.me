cfa5010f9f3f69583e7d6978ce7cc60451c61378
Satoshi Nakamoto <satoshin@gmx.com>
2023-12-17 14:01:59 -0500

    add Akash stuff

---
+++ cosmos.pilled.me/akash/entrypoint.sh
@@ -0,0 +1,9 @@
+# Bash 'Strict Mode'
+# http://redsymbol.net/articles/unofficial-bash-strict-mode
+# https://github.com/xwmx/bash-boilerplate#bash-strict-mode
+set -o nounset
+set -o errexit
+set -o pipefail
+IFS=$'\n\t'
+
+echo $AKASH_WALLET_SEED | akash keys add --recover $AKASH_KEY_NAME
